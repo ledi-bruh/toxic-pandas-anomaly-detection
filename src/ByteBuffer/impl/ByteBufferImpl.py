ac053da7dbfed0d7aedd37dcfe4da2a550e51e81
alexandr malitskiy <cfif5665@gmail.com>
2024-08-30 22:10:37 +0600

    refactoring

--- src/ByteBuffer/impl/ByteBufferImpl.py
+++ src/ByteBuffer/impl/ByteBufferImpl.py
@@ -1,24 +1,35 @@
 class ByteBufferImpl:
-    def __init__(self, size):
-        self.buffer = bytearray(size)
-        self.size = size
+    def __init__(self):
         self.position = 0
+        self.buffer = bytearray()
         self.length = 0
 
-    def write(self, data: bytes):
-        data_len = len(data)
-        if data_len + self.length > self.size:
-            raise ValueError("Not enough space in buffer")
-        self.buffer[self.length:self.length + data_len] = data
-        self.length += data_len
+    def write(self, data):
+        if isinstance(data, list):
+            for chunk in data:
+                if isinstance(chunk, bytes):
+                    self.buffer.extend(chunk)
+                    self.length += len(chunk)
+                else:
+                    raise ValueError("All elements in the list must be of type 'bytes'")
+        elif isinstance(data, bytes):
+            self.buffer.extend(data)
+            self.length += len(data)
+        else:
+            raise ValueError("Data must be of type 'bytes' or 'list of bytes'")
 
-    def read(self, window_size):
+    def read(self, window_size, num_channels):
         if window_size > self.length:
             raise ValueError("Window exceeds buffer size")
+
         window = self.buffer[:window_size]
-        self.buffer = self.buffer[window_size:] + bytearray(window_size)  # Сдвигаем оставшиеся данные в начало
+        self.buffer = self.buffer[window_size:]
         self.length -= window_size
-        return window
+
+        # Разделение данных на каналы
+        channels = [window[i::num_channels] for i in range(num_channels)]
+
+        return channels
 
     def seek(self, position):
         if position < 0 or position >= self.length:
@@ -26,5 +37,7 @@ class ByteBufferImpl:
         self.position = position
 
     def reset(self):
+        self.buffer = bytearray()
         self.position = 0
         self.length = 0
+
