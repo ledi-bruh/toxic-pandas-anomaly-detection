a4d6c814d2886c039e0fac44070826022c1d71e0
alexandr malitskiy <cfif5665@gmail.com>
2024-08-31 04:02:22 +0600

    add wav reader

--- src/anomaly_detection/application/plugin.py
+++ src/anomaly_detection/application/plugin.py
@@ -12,11 +12,6 @@ from .worker import Worker
 __all__ = ['anomaly_detection_presentation_plugin']
 
 
-async def s(a):
-    while 1:
-        await asyncio.sleep(1)
-        a.write(np.ones((8, 22050 * 3), dtype=np.float32).tobytes())
-
 
 async def anomaly_detection_presentation_plugin() -> AsyncGenerator:
     worker = Worker(
--- src/py_audio_streamer/impl/py_audio_streamer_impl.py
+++ src/py_audio_streamer/impl/py_audio_streamer_impl.py
@@ -6,7 +6,8 @@ from src.py_audio_streamer.core.py_audio_streamer import PyAudioStreamer
 
 
 class PyAudioStreamerImpl(PyAudioStreamer):
-    def __init__(self, buffer: ByteBuffer, rate=22050, chunk_size=22050):
+    def __init__(self, buffer: ByteBuffer, rate=22050, chunk_size=22050, chanals = 8):
+        self.chanals = chanals
         self.buffer = buffer
         self.rate = rate
         self.chunk_size = chunk_size
@@ -21,7 +22,7 @@ class PyAudioStreamerImpl(PyAudioStreamer):
         try:
             while True:
                 data = self.stream.read(self.chunk_size)
-                self.buffer.write([data])
+                self.buffer.write(data * self.chanals)
         except KeyboardInterrupt:
             self.stop_streaming()
 
